96a5beeb9a2fec49b8ccb6ad92eb95dfb5d2d90b
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-04-13 02:37:48 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my", "your"];
+  let adjective = ["great", "big", "spectacular", "amazing"];
+  let noun = ["jogger", "raccoon", "bandit", "magician"];
+  let url = [".com", ".net", ".io", ".edu"];
+
+  console.log("Regular for loop:");
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; ++pronounIndex) {
+    for (let adjIndex = 0; adjIndex < adjective.length; ++adjIndex) {
+      for (let nounIndex = 0; nounIndex < noun.length; ++nounIndex) {
+        for (let urlIndex = 0; urlIndex < url.length; ++urlIndex) {
+          console.log(
+            `${pronoun[pronounIndex]}${adjective[adjIndex]}${noun[nounIndex]}${url[urlIndex]}`
+          );
+        }
+      }
+    }
+  }
+
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; ++pronounIndex) {
+    for (let adjIndex = 0; adjIndex < adjective.length; ++adjIndex) {
+      for (let nounIndex = 0; nounIndex < noun.length; ++nounIndex) {
+        for (let urlIndex = 0; urlIndex < url.length; ++urlIndex) {
+          let para = document.createElement("p");
+          para.innerHTML = `${pronoun[pronounIndex]}${adjective[adjIndex]}${noun[nounIndex]}${url[urlIndex]}`;
+          document.getElementById("domain-gen").appendChild(para);
+        }
+      }
+    }
+  }
 };
